9463fe934d0126bf10a0613c8ad6de2d56da0173
Charley817 <u15165622@tuks.co.za>
2021-01-23 22:54:22 +0200

    Java OOP Initialized

---
+++ JavaOOP/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Account{
+
+}
+
+class SavingsAccount extends Account{
+
+}
+
+class CheckingAccount extends Account{
+
+}
+
+public class Main{
+  public static void main(String[] args){
+    System.out.println("Hello World");
+  }
+}
